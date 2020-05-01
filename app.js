bc1f25e7d04957f6ee35bea8f2d4ca635313223f
Irina <irina@Irinas-MacBook-Air.local>
2020-05-01 17:10:44 -0400

    Add procfile and update ports

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 
 
 
-mongoose.connect("mongodb+srv://admin-irina:hewwo@cluster0-xpmes.mongodb.net/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://admin-irina:hewwo@cluster0-xpmes.mongodb.net/todolistDB");
 
 const itemSchema = new mongoose.Schema({
   name: String
@@ -136,6 +136,11 @@ app.get("/:otherList", function(req, res) {
   });
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started successfully!");
 });
